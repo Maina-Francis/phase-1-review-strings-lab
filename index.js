06d661a9b99d47937f8908c537a64562c2fea691
Francis Maina <sebastianfrancix@gmail.com>
2022-08-21 19:38:39 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+const currentUser = "Francis Maina";
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+console.log(welcomeMessage);
+console.log(shortGreeting);
